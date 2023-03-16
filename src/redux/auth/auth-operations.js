115729d059c5f1e113bf3102b2619a6b9e7e73cd
Olena Stasiuk <estasjuk@gmail.com>
2023-03-16 16:54:20 +0200

    delete warnings

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-import { signup, login, getCurrent, logout } from 'shared/services/auth';
+//import { signup, login, getCurrent, logout } from 'shared/services/auth';
 
 export const instance = axios.create({
   baseURL: 'https://connections-api.herokuapp.com',
